dab1dc6646967249ccff825c3b9960203df7fc93
Tobias Dodge <tobydodge65@gmail.com>
2024-05-06 14:45:49 -0230

    Updated code

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ let tie = null
 const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.querySelector('#message')
 const winningCombos = [
-    [0, 1, 2], 
-    [3, 4, 5], 
+    [0, 1, 2],
+    [3, 4, 5],
     [6, 7, 8],
-    [0, 3, 6], 
-    [1, 4, 7], 
+    [0, 3, 6],
+    [1, 4, 7],
     [2, 5, 8],
-    [0, 4, 8], 
+    [0, 4, 8],
     [2, 4, 6]
 ]
 
